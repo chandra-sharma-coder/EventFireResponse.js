6f99496ce5f961e05e5fd9d9e655e2ba2e9bb37c
Chandra Bhushan Sharma <102501247+chandra-sharma-coder@users.noreply.github.com>
2022-04-19 15:21:42 +0530

    Create event.js

---
+++ event.js
@@ -0,0 +1,28 @@
+//Import Events Module
+var events = require('events');
+
+//Create and eventEmitter object
+var eventEmitter = new events.EventEmitter();
+
+//Create an Event handler as fllows;
+var connectHandler = function connected() { 
+    console.log('connection successful./......111');
+
+    // Fire the data_received event 
+    eventEmitter.emit('data_received');
+}
+
+//Bind the connection event with the handler
+eventEmitter.on('connection', connectHandler);
+
+// Bind the data_received event with the anonymous function
+eventEmitter.on('data_received', function(){
+    console.log('Data received Successfully....44444444');
+});
+
+//Fire the connection event 
+eventEmitter.emit('connection');
+
+console.log('Program Ended.');
+
+
